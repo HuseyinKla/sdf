dd79111c9c94644386b5de52a585aa284078849c
HuseyinKla <huseyin.kla1@gmail.com>
2024-11-22 19:15:44 +0300

    ş

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -29,11 +29,6 @@ app.MapGet("/weatherforecast", () =>
                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                     Random.Shared.Next(-20, 55),
                     summaries[Random.Shared.Next(summaries.Length)]
-                    
-                    
-                    
-                    
-                    
                 ))    
             .ToArray();
         return forecast;
